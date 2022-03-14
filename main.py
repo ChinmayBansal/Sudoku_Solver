a26d68acb6829a5fee889a87526fab212521c935
Chinmay Bansal <chinmaybansalsc@outlook.com>
2022-03-14 12:21:39 -0700

    Created GUI and finalized project

--- main.py
+++ main.py
@@ -36,3 +36,22 @@ def valid(board, num, pos):
             if board[i][j] == num and (i,j) != pos:
                 return False
     return True
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j% 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i,j)
+    return None
